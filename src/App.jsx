f7ce0dc66019a96995668a26a187f1f72fdcf2b8
Aathi <aathithirunav@gmail.com>
2026-06-15 22:23:12 +0530

    feat: implement main App component with chat functionality

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import { ChatInput } from './components/ChatInput';
+import { ChatMessages } from './components/ChatMessages';
+import { Chatbot } from 'supersimpledev';
+import kiss from './assets/kiss.png';
+import './App.css' 
+
+   
+function App(){
+    const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
+
+    useEffect(() => {
+        Chatbot.addResponses({
+            'hi': 'hii, welcome back',
+            'suck my dick': 'Sure , remove your pants',
+            'is it pink': 'yes , it is pink can you taste it',
+            'wtf': 'yeah yeah , fuck me hard oh yes ahhhhhhhhhhh',
+            'kiss me': function(){
+                return (
+                   <span>yeah sure 
+                    <img className='kiss' src={kiss} />
+                    </span> 
+                )
+            },
+            'goodbye': 'Goodbye. Have a great day!',
+            'give me a unique id': function(){
+                return `Sure! Here's a unique ID: ${crypto.randomUUID()} `;
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('messages', JSON.stringify(chatMessages));
+    }, [chatMessages]);
+
+    return (
+        <div className="app-container">
+            
+            <ChatMessages 
+                chatMessages = {chatMessages}
+            />
+
+            <p className="default-welcome-message"> {chatMessages.length === 0 && 'Welcome to the chatbot! Send a message using the textbox below.'} </p>
+
+            <ChatInput 
+                chatMessages = {chatMessages}
+                setChatMessages = {setChatMessages}  
+            />
+            
+        </div>
+    );
+        
+    }
+
+export default App
