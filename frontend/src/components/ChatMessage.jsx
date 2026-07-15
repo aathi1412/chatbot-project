c99c04568a0bb3108624ec37fb5242ff1c4e2849
Aathi <aathithirunav@gmail.com>
2026-07-15 16:09:45 +0530

    fix(frontend): Add alt text to images
    - Added alt text to robot and user profile images for accessibility

--- frontend/src/components/ChatMessage.jsx
+++ frontend/src/components/ChatMessage.jsx
@@ -8,22 +8,36 @@ export function ChatMessage({message, sender, time}){
     
         return (
             <div 
-                className={sender==='user' ? 'chat-message-user' : 'chat-message-robot'}
+                className={sender==='user'
+                    ? 'chat-message-user'
+                    : 'chat-message-robot'}
             >
                 {sender === 'robot' && (
-                    <img src={RobotProfileImage} width="45" className="chat-message-profile"/>
+                    <img
+                        src={RobotProfileImage}
+                        width="45"
+                        className="chat-message-profile"
+                        alt="robot profile image"
+                    />
                 )}
+
                 <div className="chat-message-text">
-                {message}
-                {time && (
-                    <div className='chat-message-time'>
-                        {dayjs(time).format('HH: mma')}
-                    </div>
-                )}
+                    {message}
+
+                    {time && (
+                        <div className='chat-message-time'>
+                            {dayjs(time).format('HH: mma')}
+                        </div>
+                    )}
                 </div>
 
                 {sender === 'user' && (
-                    <img src={userProfileImage} width="45" className="chat-message-profile"/>
+                    <img
+                        src={userProfileImage}
+                        width="45"
+                        className="chat-message-profile"
+                        alt="user profile image"
+                    />
 
                 )}
             </div>
