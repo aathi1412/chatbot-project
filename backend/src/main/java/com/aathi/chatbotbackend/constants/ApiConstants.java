336331dba809e21230b63ae7137f561c99863a4e
Aathi <aathithirunav@gmail.com>
2026-07-15 17:08:25 +0530

    security: remove exposed api token

--- backend/src/main/java/com/aathi/chatbotbackend/constants/ApiConstants.java
+++ backend/src/main/java/com/aathi/chatbotbackend/constants/ApiConstants.java
@@ -1,9 +1,10 @@
 package com.aathi.chatbotbackend.constants;
 
+
+
+
 public class ApiConstants {
+    public static final String URI = "https://router.huggingface.co/v1/chat/completions";
 
-    private  ApiConstants() {}
-
-    public static  final String URI = "https://router.huggingface.co/v1/chat/completions";
-    public static final String API_KEY = "REMOVED_HF_TOKEN";
+    private ApiConstants() {}
 }
