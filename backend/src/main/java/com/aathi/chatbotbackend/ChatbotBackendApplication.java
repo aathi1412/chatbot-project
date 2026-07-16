097de7a37a2bac5896edb2f4590359a5872a1487
Aathi <aathithirunav@gmail.com>
2026-07-16 21:15:02 +0530

    refactor(backend): Enable Configuration Properties Scan
    - Added @ConfigurationPropertiesScan annotation to ChatbotBackendApplication

--- backend/src/main/java/com/aathi/chatbotbackend/ChatbotBackendApplication.java
+++ backend/src/main/java/com/aathi/chatbotbackend/ChatbotBackendApplication.java
@@ -2,8 +2,10 @@ package com.aathi.chatbotbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ChatbotBackendApplication {
 
     public static void main(String[] args) {
