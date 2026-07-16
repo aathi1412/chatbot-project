00d1950c659e8b0f6895f5dbd5ec21d0ed170a6f
Aathi <aathithirunav@gmail.com>
2026-07-16 21:14:51 +0530

    refactor(backend): Remove unused constants
    - Removed unused URI constant
    - Removed private constructor

--- backend/src/main/java/com/aathi/chatbotbackend/constants/ApiConstants.java
+++ backend/src/main/java/com/aathi/chatbotbackend/constants/ApiConstants.java
@@ -1,10 +1,5 @@
 package com.aathi.chatbotbackend.constants;
 
 
-
-
 public class ApiConstants {
-    public static final String URI = "https://router.huggingface.co/v1/chat/completions";
-
-    private ApiConstants() {}
 }
