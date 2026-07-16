ffad49cee70fbb91a679b3cc339e8d7e8cc61d61
Aathi <aathithirunav@gmail.com>
2026-07-16 21:15:08 +0530

    feat(backend): Add security configuration
    - Implement security filter chain
    - Configure CORS settings
    - Define allowed origins, headers, and methods

--- backend/src/main/java/com/aathi/chatbotbackend/config/securityconfig.java
+++ backend/src/main/java/com/aathi/chatbotbackend/config/securityconfig.java
@@ -2,13 +2,60 @@ package com.aathi.chatbotbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class securityconfig {
+public class securityConfig {
 
     @Bean
     public RestClient restClient(){
         return RestClient.create();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/chat/ask-ai",
+                                "/api/v1/chat/ask-image",
+                                "/api/v1/chat"
+                        ).permitAll()
+
+                )
+
+                .build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(
+                List.of("http://localhost:5173")
+        );
+        corsConfiguration.setAllowedHeaders(
+                List.of("*")
+        );
+        corsConfiguration.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        );
+
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
