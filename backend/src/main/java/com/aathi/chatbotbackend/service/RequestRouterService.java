bc17a6df8ec33c7e36bbe6040875b5dfcf614be4
Aathi <aathithirunav@gmail.com>
2026-07-16 21:23:45 +0530

    feat(backend): Add RequestRouterService
    - Routes incoming requests to the appropriate service based on the classified intent
    - Supports text and image generation services

---
+++ backend/src/main/java/com/aathi/chatbotbackend/service/RequestRouterService.java
@@ -0,0 +1,24 @@
+package com.aathi.chatbotbackend.service;
+
+import com.aathi.chatbotbackend.dto.response.ChatResponse;
+import com.aathi.chatbotbackend.enums.ToolType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RequestRouterService {
+
+    private final IntentClassifierService intentClassifier;
+    private final TextGenerationService textGenerationService;
+    private final ImageGenerationService imageGenerationService;
+    public ChatResponse routeRequest(String prompt) {
+
+        ToolType tool = intentClassifier.classifyPrompt(prompt);
+
+        return switch (tool) {
+            case TEXT -> textGenerationService.generateChatResponse(prompt);
+            case IMAGE ->  imageGenerationService.generateImage(prompt);
+        };
+    }
+}
