2f5a4dfd30e5c56e78899636afe7808a21fff5a1
Aathi <aathithirunav@gmail.com>
2026-07-17 23:01:00 +0530

    feat(backend): Add BadRequestException
    - Create a custom BadRequestException class to handle bad request errors

---
+++ backend/src/main/java/com/aathi/chatbotbackend/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.aathi.chatbotbackend.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
